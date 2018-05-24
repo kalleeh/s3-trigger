d5c5e1ba9526fd6b0cea261d80d46be9b69f1e8d
kalleh <wallbomk@amazon.com>
2018-05-24 18:24:27 +0200

    commit

--- index.py
+++ index.py
@@ -1,12 +1,24 @@
+
 import json
-import datetime
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
 
 
-def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = 'aws-ps-boe'
+    key = urllib.parse.unquote_plus('art.rtf', encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
